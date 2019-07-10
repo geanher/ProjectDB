cc07d10d249083985fd7f9397866a8b34b38a7ab
Gerly Andres Hernandez <gahernandez12@gmail.com>
2019-07-10 01:22:43 -0400

    edited Products (added update and edit)

--- ProjectDB/productos/urls.py
+++ ProjectDB/productos/urls.py
@@ -1,4 +1,4 @@
-from productos.views import productos, producto_view, productos_list
+from productos.views import productos, producto_view, productos_list, producto_edit, producto_delete
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', login_required(productos), name='productos'),
     path('nuevo', login_required(producto_view), name="productos_crear"),
     path('listar', login_required(productos_list), name='producto_listar'),
+    path('editar/<int:codigo>/' , login_required(producto_edit), name='producto_editar'),
+    path('eliminar/<int:codigo>/' , login_required(producto_delete), name='producto_eliminar'),
 
 
 ]
--- ProjectDB/productos/views.py
+++ ProjectDB/productos/views.py
@@ -26,3 +26,24 @@ def productos_list(request):
 	producto = Productos.objects.all()
 	contexto = {'productos' :producto}
 	return render(request, 'productos/productos_list.html', contexto)
+
+
+def producto_edit(request, codigo):
+	producto = Productos.objects.get(codigo=codigo)
+	if request.method == 'GET':
+		form = ProductosForm(instance=producto)
+	else:
+		form = ProductosForm(request.POST, instance=producto)
+		if form.is_valid():
+			form.save()
+		return redirect('productos:producto_listar')
+	return render(request, 'productos/productos_form.html', {'form':form})
+
+
+def producto_delete(request, codigo):
+	producto = Productos.objects.get(codigo = codigo)
+	if request.method == 'POST' :
+		producto.delete()
+		return redirect('productos:producto_listar')
+	return render(request, 'productos/producto_delete.html', {'producto' :producto})
+	
